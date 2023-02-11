3c1cc197697d149b9e5f3df536fa3a556d986b63
SamLong01 <samueladebayo1015@gmail.com>
2023-02-11 01:11:46 +0100

    mandatory

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""class that inherit from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """class that inherit from BaseModel"""
+    state_id = ""
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""class that inherit from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """class that inherit from BaseModel"""
+    place_id = ""
+    user_id = ""
+    text = ""
